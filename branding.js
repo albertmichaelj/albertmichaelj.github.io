c19310f4dec2052bcb8d34d10fdbbe317e0b6a76
Michael Albert <albertmichaelj@gmail.com>
2017-09-30 18:17:51 -0400

    My old website added.

---
+++ branding.js
@@ -0,0 +1,3 @@
+function malbertfooter(){
+	document.write('<a target="_blank" href="http://www.duke.edu"><img src="Duke_University_logo.png" border="100" alt="Duke University" width="30%" height="30%" /></a>');	
+}
